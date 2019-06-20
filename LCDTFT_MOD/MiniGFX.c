daa1bd81280e211919ab0e4f69f1f5857663a870
intenseC <51984663+intenseC@users.noreply.github.com>
2019-06-20 14:21:44 +0300

    Update MiniGFX.c

--- LCDTFT_MOD/MiniGFX.c
+++ LCDTFT_MOD/MiniGFX.c
@@ -83,55 +83,9 @@ void _setFont(const uint8_t * font);
 void setColor(uint16_t color);
 void setBackColor(uint32_t color);
 void _printChar(char c, int x, int y);
-
-#if 0
-//////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
  /*     UTFT lib port  END    */
 //=========================================================
-   void startWrite(void);
-   void writePixel(int16_t x, int16_t y, uint16_t color);
-   void writeFillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color);
-   void writeFastVLine(int16_t x, int16_t y, int16_t h, uint16_t color);
-   void writeFastHLine(int16_t x, int16_t y, int16_t w, uint16_t color);
-   void writeLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1, uint16_t color);
-   void endWrite(void);
-
-    void
-    drawCircle(int16_t x0, int16_t y0, int16_t r, uint16_t color),
-    drawCircleHelper(int16_t x0, int16_t y0, int16_t r, uint8_t cornername,
-      uint16_t color),
-    fillCircle(int16_t x0, int16_t y0, int16_t r, uint16_t color),
-    fillCircleHelper(int16_t x0, int16_t y0, int16_t r, uint8_t cornername,
-      int16_t delta, uint16_t color),
-    drawTriangle(int16_t x0, int16_t y0, int16_t x1, int16_t y1,
-      int16_t x2, int16_t y2, uint16_t color),
-    fillTriangle(int16_t x0, int16_t y0, int16_t x1, int16_t y1,
-      int16_t x2, int16_t y2, uint16_t color),
-    drawRoundRect(int16_t x0, int16_t y0, int16_t w, int16_t h,
-      int16_t radius, uint16_t color),
-    fillRoundRect(int16_t x0, int16_t y0, int16_t w, int16_t h,
-      int16_t radius, uint16_t color);
-
-    void drawChar(int16_t x, int16_t y, unsigned char c, uint16_t color,
-      uint16_t bg, uint8_t size),
-    setCursor(int16_t x, int16_t y),
-   // setTextColor(uint16_t c),
-    setTextColor(uint16_t c, uint16_t bg),
-    setTextSize(uint8_t s),
-    setTextWrap(boolean w),
-    cp437(boolean x),
-    	    #ifdef ADAFONTS
-	setFont(const GFXfont *f),
-            #endif
-        /* getTextBounds(const __FlashStringHelper *s, int16_t x, int16_t y,
-      int16_t *x1, int16_t *y1, uint16_t *w, uint16_t *h), */ 
-	getTextBounds(char *string, int16_t x, int16_t y,
-      int16_t *x1, int16_t *y1, uint16_t *w, uint16_t *h);
-
-
-  void   write(uint8_t);
-
-#endif
 //=========================================================
 
 
@@ -869,7 +823,7 @@ void _printChar(char c, int x, int y)
 	  }
 
 
-//void print(char * st, int x, int y)
+
 void print(char * st)
 {
     int x = cursor_x;  int y = cursor_y;
