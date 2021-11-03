606c6c24b9f6fb9a1fe9533212aad2f437d9e07d
intenseC <51984663+intenseC@users.noreply.github.com>
2021-11-03 09:25:24 +0200

    Update display.h

--- display.h
+++ display.h
@@ -20,9 +20,9 @@
 #define readReg(val)   id    // dummy placeholder
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #endif 
-	 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////			
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////			
 #define ILI9341_8BIT    /*  ILI9341 pure SPI mode ON/OFF  */
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
  
@@ -34,37 +34,35 @@
 
 
                  //Func Prototypes
-  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  void delay(unsigned int d);
-  void tft_init(void);
-  void writeRegister8(uint8_t a, uint8_t d);
-  void writeRegister16(uint16_t a, uint16_t d);
-  void writeRegisterPair(uint8_t aH, uint8_t aL, uint16_t d);
-  void begin(void);
-  void reset(void);
-  void pushColors(uint16_t *data, uint8_t len, boolean first);
-  void setRotation(uint8_t x);
-  uint16_t readPixel(int16_t x, int16_t y);
-  uint16_t readID(void);
-  uint32_t readReg(uint8_t r);
-  uint16_t color565(uint8_t r, uint8_t g, uint8_t b);
-  void writeRegister24(uint8_t r, uint32_t d);
-  void writeRegister32(uint8_t r, uint32_t d);
-  void     drawPixel(int16_t x, int16_t y, uint16_t color);
-  void     drawFastHLine(int16_t x0, int16_t y0, int16_t w, uint16_t color);
-  void     drawFastVLine(int16_t x0, int16_t y0, int16_t h, uint16_t color);
-  void     fillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t c);
-  void     fillScreen(uint16_t color);
-  void     setRegisters8(uint8_t *ptr, uint8_t n);
-  void     setRegisters16(uint16_t *ptr, uint8_t n);
-  void     setRotation(uint8_t x);
-  void     setLR(void);
-  void     setAddrWindow(int x1, int y1, int x2, int y2);
-  void     pushColors(uint16_t *data, uint8_t len, boolean first);
-  void     charBounds(char c, int16_t *x, int16_t *y,
-  int16_t *minx, int16_t *miny, int16_t *maxx, int16_t *maxy);
-  void flood(uint16_t color, uint32_t len);
-
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  void 		delay(unsigned int d);
+  void 		tft_init(void);
+  void 		writeRegister8(uint8_t a, uint8_t d);
+  void 		writeRegister16(uint16_t a, uint16_t d);
+  void 		writeRegisterPair(uint8_t aH, uint8_t aL, uint16_t d);
+  void 		begin(void);
+  void 		reset(void);
+  void 		pushColors(uint16_t *data, uint8_t len, boolean first);
+  void 		setRotation(uint8_t x);
+  uint16_t 	readPixel(int16_t x, int16_t y);
+  uint16_t 	readID(void);
+  uint32_t 	readReg(uint8_t r);
+  uint16_t 	color565(uint8_t r, uint8_t g, uint8_t b);
+  void 		writeRegister24(uint8_t r, uint32_t d);
+  void    	writeRegister32(uint8_t r, uint32_t d);
+  void     	drawPixel(int16_t x, int16_t y, uint16_t color);
+  void     	drawFastHLine(int16_t x0, int16_t y0, int16_t w, uint16_t color);
+  void     	drawFastVLine(int16_t x0, int16_t y0, int16_t h, uint16_t color);
+  void     	fillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t c);
+  void     	fillScreen(uint16_t color);
+  void     	setRegisters8(uint8_t *ptr, uint8_t n);
+  void     	setRegisters16(uint16_t *ptr, uint8_t n);
+  void     	setRotation(uint8_t x);
+  void     	setLR(void);
+  void     	setAddrWindow(int x1, int y1, int x2, int y2);
+  void     	pushColors(uint16_t *data, uint8_t len, boolean first);
+  void     	charBounds(char c, int16_t *x, int16_t *y,
+  int16_t 	*minx, int16_t *miny, int16_t *maxx, int16_t *maxy);
+  void 		flood(uint16_t color, uint32_t len);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
